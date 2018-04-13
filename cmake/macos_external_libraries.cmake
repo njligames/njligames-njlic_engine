977ee98d5cc20ed73c65a01a2c5026ce02f866e7
njligames <jamesfolk1@gmail.com>
2018-04-13 16:56:02 -0400

    fixed library

--- cmake/macos_external_libraries.cmake
+++ cmake/macos_external_libraries.cmake
@@ -1,11 +1,62 @@
 
-message(STATUS "Platform is macos for project")
+message(STATUS "Platform is ios for project")
 
-# find_package(SDL2 REQUIRED)
-# list(APPEND EXTRA_LIBS ${SDL2_LIBRARY})
-# include_directories(${SDL2_INCLUDE_DIR})
+macro(DOWNLOAD_LIBRARY_INCLUDE_FILES LIBRARY_INCLUDE_URL LIBRARY_DIRECTORY)
+
+  set(${CMAKE_PROJECT_NAME}_URL_INCLUDE 
+    "${LIBRARY_INCLUDE_URL}" 
+    CACHE STRING "The URL for the include files for ${LIBRARY_DIRECTORY}")
+
+  RETRIEVE_TAR(
+    "${${CMAKE_PROJECT_NAME}_URL_INCLUDE}"
+    "${LIBRARY_DIRECTORY}"
+    "SKIP"
+    )
+
+  include_directories("${CMAKE_BINARY_DIR}/${LIBRARY_DIRECTORY}")
+
+endmacro(DOWNLOAD_LIBRARY_INCLUDE_FILES LIBRARY_INCLUDE_URL LIBRARY_DIRECTORY)
+
+DOWNLOAD_LIBRARY_INCLUDE_FILES("https://www.dropbox.com/s/bvtxtpqa925godz/BULLET-1.85_include.tar.gz?dl=0" "thirdparty/bullet-1.85/include")
+
+# set(${CMAKE_PROJECT_NAME}_URL_SDL2_INCLUDE 
+#   "https://www.dropbox.com/s/g0qrc1xkz2qcb9y/SDL2-2.0.7_include.tar.gz?dl=0" 
+#   CACHE STRING "The URL for the include files for SDL2")
 # 
-# find_package(GLEW REQUIRED)
-# list(APPEND EXTRA_LIBS ${GLEW_LIBRARY})
-# include_directories(${GLEW_INCLUDE_DIR})
+# set(${CMAKE_PROJECT_NAME}_URL_SDL2_LIB_IOS_RELEASE 
+#   "https://www.dropbox.com/s/xsvgzfp2h4lk965/SDL2-2.0.7_ios.tar.gz?dl=0" 
+#   CACHE STRING "The URL for the iOS version of SDL2"
+#   )
+# 
+# set(SDL2_INCLUDE_DIR "thirdparty/SDL2-2.0.7/include")
+# set(LIBSDL_INCLUDE_URL "${${CMAKE_PROJECT_NAME}_URL_SDL2_INCLUDE}")
+# 
+# RETRIEVE_TAR(
+#   "${LIBSDL_INCLUDE_URL}"
+#   "${SDL2_INCLUDE_DIR}"
+#   "SKIP"
+#   )
+# 
+# include_directories("${CMAKE_BINARY_DIR}/${SDL2_INCLUDE_DIR}")
 
+
+
+
+
+
+
+
+# set(LIBSDL_LIBRARY_URL "${${CMAKE_PROJECT_NAME}_URL_SDL2_LIB_IOS_RELEASE}")
+# set(LIBSDL_LIBRARY_TARGET_DIRECTORY "thirdparty/SDL2-2.0.7/lib/ios")
+# 
+# RETRIEVE_TAR(
+#   "${LIBSDL_LIBRARY_URL}"
+#   "${LIBSDL_LIBRARY_TARGET_DIRECTORY}"
+#   "SKIP"
+#   )
+# 
+# set(SDL_IMPORT_LOCATION "${CMAKE_BINARY_DIR}/${LIBSDL_LIBRARY_TARGET_DIRECTORY}/Release\$(EFFECTIVE_PLATFORM_NAME)/libSDL2.${STATIC_LIBRARY_EXTENSION}")
+# 
+# add_library(SDL2_LIBRARY STATIC IMPORTED )
+# set_target_properties(SDL2_LIBRARY PROPERTIES IMPORTED_LOCATION "${SDL_IMPORT_LOCATION}")
+# list(APPEND EXTRA_LIBS SDL2_LIBRARY)
