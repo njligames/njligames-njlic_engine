58258a5da1ac8074a20eae8c13a5f6d962861db5
njligames <jamesfolk1@gmail.com>
2018-04-17 17:06:30 -0400

    fixed the sound defines

--- njli/platform/macOS/SoundPlatform.h
+++ njli/platform/macOS/SoundPlatform.h
@@ -1,10 +1,10 @@
-#if defined(NJLI_SOUND_FMOD)
+#if defined(__FMOD__)
 #include "fmod.hpp"
 #include "fmod_errors.h"
-#elif defined(NJLI_SOUND_OPENAL)
+#elif defined(__OPENAL__)
 #include <OpenAL/al.h>
 #include <OpenAL/alc.h>
-#elif defined(NJLI_SOUND_SDL)
+#elif defined(__SDL__)
 #include "SDL_audio.h"
 #else
 #error Unsupported choice setting
