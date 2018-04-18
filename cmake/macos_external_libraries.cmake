cff49bf3b605238898598c30a27ff735bd2f7ed5
James Folk <james.folk@envrmnt.com>
2018-04-18 14:32:54 -0400

    put back the macros

--- cmake/macos_external_libraries.cmake
+++ cmake/macos_external_libraries.cmake
@@ -1,6 +1,62 @@
 
 message(STATUS "Platform is macOS for project")
 
+macro(DOWNLOAD_LIBRARY_INCLUDE_FILES LIBNAME LIBRARY_INCLUDE_URL LIBRARY_DIRECTORY)
+
+  set(${LIBNAME}_URL_INCLUDE 
+    "${LIBRARY_INCLUDE_URL}" 
+    CACHE STRING "The URL for the include files for ${LIBRARY_DIRECTORY}")
+
+  RETRIEVE_TAR(
+    "${${LIBNAME}_URL_INCLUDE}"
+    "${LIBRARY_DIRECTORY}"
+    "SKIP"
+    )
+
+  SET(SUBDIRS "")
+  SUBDIRLIST(SUBDIRS "${CMAKE_BINARY_DIR}/${LIBRARY_DIRECTORY}" ${LIBNAME}_INCLUDE_DIRECTORY_LIST)
+
+  include_directories("${CMAKE_BINARY_DIR}/${LIBRARY_DIRECTORY}" ${${LIBNAME}_INCLUDE_DIRECTORY_LIST})
+
+endmacro()
+
+
+macro(DOWNLOAD_LIBRARY_BINARY_FILES LIBNAME LIBRARY_BINARY_URL LIBRARY_DIRECTORY)
+
+  set(${LIBNAME}_URL_BINARY 
+    "${LIBRARY_BINARY_URL}" 
+    CACHE STRING "The URL for the include files for ${LIBRARY_DIRECTORY}")
+
+  RETRIEVE_TAR(
+    "${${LIBNAME}_URL_BINARY}"
+    "${LIBRARY_DIRECTORY}"
+    "SKIP"
+    )
+
+  MESSAGE(STATUS "library directory ${CMAKE_BINARY_DIR}/${LIBRARY_DIRECTORY}/Release/")
+  file(GLOB_RECURSE ${LIBNAME}_RELEASE_BINARY_FILES
+    ${CMAKE_BINARY_DIR}/${LIBRARY_DIRECTORY}/Release/*.a
+    )
+
+  MESSAGE(STATUS "_RELEASE_BINARY_FILES ${${LIBNAME}_RELEASE_BINARY_FILES}")
+
+  foreach(BINARY_FILE ${${LIBNAME}_RELEASE_BINARY_FILES})
+    string(FIND ${BINARY_FILE} "/" _IDXSLASH_ REVERSE)
+    string(FIND ${BINARY_FILE} "." _IDXDOT_ REVERSE)
+
+    MATH(EXPR _BEGIN_ "${_IDXSLASH_}+1")
+    MATH(EXPR _LENGTH_ "${_IDXDOT_}-${_IDXSLASH_}-1")
+
+    string(SUBSTRING ${BINARY_FILE} ${_BEGIN_} ${_LENGTH_} _LIB_NAME_)
+
+    MESSAGE(STATUS "BINARY_FILE ${BINARY_FILE} - _LIB_NAME_ ${_LIB_NAME_}")
+
+    add_library(${_LIB_NAME_}_LIBRARY STATIC IMPORTED )
+    set_target_properties(${_LIB_NAME_}_LIBRARY PROPERTIES IMPORTED_LOCATION "${BINARY_FILE}")
+    list(APPEND EXTRA_LIBS ${_LIB_NAME_}_LIBRARY)
+  endforeach()
+
+endmacro()
 
 DOWNLOAD_LIBRARY_INCLUDE_FILES("bullet" "https://www.dropbox.com/s/bvtxtpqa925godz/BULLET-1.85_include.tar.gz?dl=0" "thirdparty/bullet/include")
 DOWNLOAD_LIBRARY_INCLUDE_FILES("debug-draw" "https://www.dropbox.com/s/isuefnerr26na7p/LIBDEBUGDRAW_include.tar.gz?dl=0" "thirdparty/debug-draw/include")
