56878132dde6b4a1a9a67e9b8136d8ec5757e9b0
James Folk <james.folk@envrmnt.com>
2018-04-20 08:53:47 -0400

    added easy logger

---
+++ njli/platform/easylogger.cpp
@@ -0,0 +1,19 @@
+//
+//  easylogger.cpp
+//  EasyLogger-Test
+//
+//  Created by James Folk on 4/19/18.
+//
+
+#include "easylogger.h"
+
+namespace easylogger {
+  namespace _private {
+    
+    _private::LogSink::~LogSink() {
+        _logger->WriteLog(Level(), _logger, _file, _line, _func,
+                          _os.str().c_str());
+    }
+  }
+    
+} // namespace easylogger
