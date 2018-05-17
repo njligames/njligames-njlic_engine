7a5cff44661ba5ed0a4d0198cfe8447100362e1b
James Folk <james@njligames.com>
2018-05-17 10:10:50 -0400

    fixed graphics platform for emscripten

--- njli/platform/emscripten/GraphicsPlatform.h
+++ njli/platform/emscripten/GraphicsPlatform.h
@@ -1,20 +1,20 @@
 
-#if defined(NJLI_GL_ES2)
+#if defined(__GL_ES2__)
     #define GL_GLEXT_PROTOTYPES
     #include <GLES2/gl2.h>
     #include <GLES2/gl2ext.h>
     #include <GLES2/gl2platform.h>
     #define NANOVG_GLES2_IMPLEMENTATION
-#elif defined(NJLI_GL_ES3)
+#elif defined(__GL_ES3__)
     #include <GLES3/gl3.h>
     #include <GLES3/gl2ext.h>
     #include <GLES3/gl3platform.h>
     #include <GLES3/gl31.h>
     #include <GLES3/gl32.h>
     #define NANOVG_GLES3_IMPLEMENTATION
-#elif defined(NJLI_GL_2)
+#elif defined(__GL_2__)
 #error Unsupported choice setting
-#elif defined(NJLI_GL_3)
+#elif defined(__GL_3__)
     #error Unsupported choice setting
 #else
     #error Unsupported choice setting
