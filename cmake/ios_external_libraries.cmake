3e4a2fe3fd5065afa2638529215d1c225821565b
James Folk <james@njligames.com>
2018-06-16 01:10:44 -0400

    added the libraries for ios

--- cmake/ios_external_libraries.cmake
+++ cmake/ios_external_libraries.cmake
@@ -2,8 +2,160 @@
 message(STATUS "Platform is ios for project")
 
 DOWNLOAD_STATIC_RELEASE_LIBRARY_BINARY_FILES("bullet"
-  "https://www.dropbox.com/s/4j68y5239qdydve/LIBBULLET_1.3.5_ios.tar.gz?dl=0"
-  "thirdparty/bullet/lib/ios")
+  "https://www.dropbox.com/s/rkyipgs1clmy58z/LIBBULLET_1.3.5_ios_Release_iphoneos.tar.gz?dl=0"
+  "thirdparty/bullet/lib/ios/Release-iphoneos"
+  "thirdparty/bullet/lib/ios/Release\$(EFFECTIVE_PLATFORM_NAME)"
+  )
+DOWNLOAD_STATIC_RELEASE_LIBRARY_BINARY_FILES("imgui"
+  "https://www.dropbox.com/s/wd4olqjsp3f6szf/LIBIMGUI_1.5.0_ios_Release_iphoneos.tar.gz?dl=0"
+  "thirdparty/imgui/lib/ios/Release-iphoneos"
+  "thirdparty/imgui/lib/ios/Release\$(EFFECTIVE_PLATFORM_NAME)"
+  )
+DOWNLOAD_STATIC_RELEASE_LIBRARY_BINARY_FILES("imguizmo"
+  "https://www.dropbox.com/s/6qep7rmssmnhn0s/LIBIMGUIZMO_1.04_ios_Release_iphoneos.tar.gz?dl=0"
+  "thirdparty/imguizmo/lib/ios/Release-iphoneos"
+  "thirdparty/imguizmo/lib/ios/Release\$(EFFECTIVE_PLATFORM_NAME)"
+  )
+DOWNLOAD_STATIC_RELEASE_LIBRARY_BINARY_FILES("jsoncpp"
+  "https://www.dropbox.com/s/pbfr4vrz4j3kz87/LIBJSONCPP_1.8.0_ios_Release_iphoneos.tar.gz?dl=0"
+  "thirdparty/jsoncpp/lib/ios/Release-iphoneos"
+  "thirdparty/jsoncpp/lib/ios/Release\$(EFFECTIVE_PLATFORM_NAME)"
+  )
+DOWNLOAD_STATIC_RELEASE_LIBRARY_BINARY_FILES("nanovg"
+  "https://www.dropbox.com/s/g5qlu9b1qqgxsvx/LIBNANOVG_ios_Release_iphoneos.tar.gz?dl=0"
+  "thirdparty/nanovg/lib/ios/Release-iphoneos"
+  "thirdparty/nanovg/lib/ios/Release\$(EFFECTIVE_PLATFORM_NAME)"
+  )
+DOWNLOAD_STATIC_RELEASE_LIBRARY_BINARY_FILES("ogg"
+  "https://www.dropbox.com/s/6yglbfds13qtbe9/LIBOGG_1.3.2_ios_Release_iphoneos.tar.gz?dl=0"
+  "thirdparty/ogg/lib/ios/Release-iphoneos"
+  "thirdparty/ogg/lib/ios/Release\$(EFFECTIVE_PLATFORM_NAME)"
+  )
+DOWNLOAD_STATIC_RELEASE_LIBRARY_BINARY_FILES("vorbis"
+  "https://www.dropbox.com/s/54cw2j8eu858c8z/LIBVORBIS_1.3.5_ios_Release_iphoneos.tar.gz?dl=0"
+  "thirdparty/vorbis/lib/ios/Release-iphoneos"
+  "thirdparty/vorbis/lib/ios/Release\$(EFFECTIVE_PLATFORM_NAME)"
+  )
+
+DOWNLOAD_STATIC_RELEASE_LIBRARY_BINARY_FILES("bullet"
+  "https://www.dropbox.com/s/gqevb4h39vhzv1d/LIBBULLET_1.3.5_ios_Release_iphonesimulator.tar.gz?dl=0"
+  "thirdparty/bullet/lib/ios/Release-iphonesimulator"
+  "thirdparty/bullet/lib/ios/Release\$(EFFECTIVE_PLATFORM_NAME)"
+  )
+DOWNLOAD_STATIC_RELEASE_LIBRARY_BINARY_FILES("imgui"
+  "https://www.dropbox.com/s/hx0si0yecpyvdsx/LIBIMGUI_1.5.0_ios_Release_iphonesimulator.tar.gz?dl=0"
+  "thirdparty/imgui/lib/ios/Release-iphonesimulator"
+  "thirdparty/imgui/lib/ios/Release\$(EFFECTIVE_PLATFORM_NAME)"
+  )
+DOWNLOAD_STATIC_RELEASE_LIBRARY_BINARY_FILES("imguizmo"
+  "https://www.dropbox.com/s/ktucv72g98cjuc1/LIBIMGUIZMO_1.04_ios_Release_iphonesiulator.tar.gz?dl=0"
+  "thirdparty/imguizmo/lib/ios/Release-iphonesimulator"
+  "thirdparty/imguizmo/lib/ios/Release\$(EFFECTIVE_PLATFORM_NAME)"
+  )
+DOWNLOAD_STATIC_RELEASE_LIBRARY_BINARY_FILES("jsoncpp"
+  "https://www.dropbox.com/s/u0c7b7w86urg80j/LIBJSONCPP_1.8.0_ios_Release_iphonesimulator.tar.gz?dl=0"
+  "thirdparty/jsoncpp/lib/ios/Release-iphonesimulator"
+  "thirdparty/jsoncpp/lib/ios/Release\$(EFFECTIVE_PLATFORM_NAME)"
+  )
+DOWNLOAD_STATIC_RELEASE_LIBRARY_BINARY_FILES("nanovg"
+  "https://www.dropbox.com/s/7haa2jdpftzfbu9/LIBNANOVG_ios_Release_iphonesimulator.tar.gz?dl=0"
+  "thirdparty/nanovg/lib/ios/Release-iphonesimulator"
+  "thirdparty/nanovg/lib/ios/Release\$(EFFECTIVE_PLATFORM_NAME)"
+  )
+DOWNLOAD_STATIC_RELEASE_LIBRARY_BINARY_FILES("ogg"
+  "https://www.dropbox.com/s/jg8oj34xcwnlyxp/LIBOGG_1.3.2_ios_Release_iphonesimulator.tar.gz?dl=0"
+  "thirdparty/ogg/lib/ios/Release-iphonesimulator"
+  "thirdparty/ogg/lib/ios/Release\$(EFFECTIVE_PLATFORM_NAME)"
+  )
+DOWNLOAD_STATIC_RELEASE_LIBRARY_BINARY_FILES("vorbis"
+  "https://www.dropbox.com/s/c9bwtzf7ar5ty0j/LIBVORBIS_1.3.5_ios_Release_iphonesimulator.tar.gz?dl=0"
+  "thirdparty/vorbis/lib/ios/Release-iphonesimulator"
+  "thirdparty/vorbis/lib/ios/Release\$(EFFECTIVE_PLATFORM_NAME)"
+  )
+
+
+
+
+
+DOWNLOAD_STATIC_DEBUG_LIBRARY_BINARY_FILES("bullet"
+  "https://www.dropbox.com/s/54tk9c2rr6lfssr/LIBBULLET_1.3.5_ios_Debug_iphoneos.tar.gz?dl=0"
+  "thirdparty/bullet/lib/ios/Debug-iphoneos"
+  "thirdparty/bullet/lib/ios/Debug\$(EFFECTIVE_PLATFORM_NAME)"
+  )
+DOWNLOAD_STATIC_DEBUG_LIBRARY_BINARY_FILES("imgui"
+  "https://www.dropbox.com/s/k4wsy0qgwa4ki4e/LIBIMGUI_1.5.0_ios_Debug_iphoneos.tar.gz?dl=0"
+  "thirdparty/imgui/lib/ios/Debug-iphoneos"
+  "thirdparty/imgui/lib/ios/Debug\$(EFFECTIVE_PLATFORM_NAME)"
+  )
+DOWNLOAD_STATIC_DEBUG_LIBRARY_BINARY_FILES("imguizmo"
+  "https://www.dropbox.com/s/h4bt6u99t8duujn/LIBIMGUIZMO_1.04_ios_Debug_iphoneos.tar.gz?dl=0"
+  "thirdparty/imguizmo/lib/ios/Debug-iphoneos"
+  "thirdparty/imguizmo/lib/ios/Debug\$(EFFECTIVE_PLATFORM_NAME)"
+  )
+DOWNLOAD_STATIC_DEBUG_LIBRARY_BINARY_FILES("jsoncpp"
+  "https://www.dropbox.com/s/cu18b33lhd40scy/LIBJSONCPP_1.8.0_ios_Debug_iphoneos.tar.gz?dl=0"
+  "thirdparty/jsoncpp/lib/ios/Debug-iphoneos"
+  "thirdparty/jsoncpp/lib/ios/Debug\$(EFFECTIVE_PLATFORM_NAME)"
+  )
+DOWNLOAD_STATIC_DEBUG_LIBRARY_BINARY_FILES("nanovg"
+  "https://www.dropbox.com/s/fvl7zq23tk4b6m1/LIBNANOVG_ios_Debug_iphoneos.tar.gz?dl=0"
+  "thirdparty/nanovg/lib/ios/Debug-iphoneos"
+  "thirdparty/nanovg/lib/ios/Debug\$(EFFECTIVE_PLATFORM_NAME)"
+  )
+DOWNLOAD_STATIC_DEBUG_LIBRARY_BINARY_FILES("ogg"
+  "https://www.dropbox.com/s/u8put61k81b7psy/LIBOGG_1.3.2_ios_Debug_iphoneos.tar.gz?dl=0"
+  "thirdparty/ogg/lib/ios/Debug-iphoneos"
+  "thirdparty/ogg/lib/ios/Debug\$(EFFECTIVE_PLATFORM_NAME)"
+  )
+DOWNLOAD_STATIC_DEBUG_LIBRARY_BINARY_FILES("vorbis"
+  "https://www.dropbox.com/s/0oy4onslwbzqf8q/LIBVORBIS_1.3.5_ios_Debug_iphoneos.tar.gz?dl=0"
+  "thirdparty/vorbis/lib/ios/Debug-iphoneos"
+  "thirdparty/vorbis/lib/ios/Debug\$(EFFECTIVE_PLATFORM_NAME)"
+  )
+
+
+
+
+
+DOWNLOAD_STATIC_DEBUG_LIBRARY_BINARY_FILES("bullet"
+  "https://www.dropbox.com/s/ew41x375h4bd3fq/LIBBULLET_1.3.5_ios_Debug_iphonesimulator.tar.gz?dl=0"
+  "thirdparty/bullet/lib/ios/Debug-iphonesimulator"
+  "thirdparty/bullet/lib/ios/Debug\$(EFFECTIVE_PLATFORM_NAME)"
+  )
+DOWNLOAD_STATIC_DEBUG_LIBRARY_BINARY_FILES("imgui"
+  "https://www.dropbox.com/s/ikq8z8vk0f0ycnd/LIBIMGUI_1.5.0_ios_Debug_iphonesimulator.tar.gz?dl=0"
+  "thirdparty/imgui/lib/ios/Debug-iphonesimulator"
+  "thirdparty/imgui/lib/ios/Debug\$(EFFECTIVE_PLATFORM_NAME)"
+  )
+DOWNLOAD_STATIC_DEBUG_LIBRARY_BINARY_FILES("imguizmo"
+  "https://www.dropbox.com/s/7xkynw8b7wkg4bv/LIBIMGUIZMO_1.04_ios_Debug_iphonesimulator.tar.gz?dl=0"
+  "thirdparty/imguizmo/lib/ios/Debug-iphonesimulator"
+  "thirdparty/imguizmo/lib/ios/Debug\$(EFFECTIVE_PLATFORM_NAME)"
+  )
+DOWNLOAD_STATIC_DEBUG_LIBRARY_BINARY_FILES("jsoncpp"
+  "https://www.dropbox.com/s/5hy7eahoe2fodp3/LIBJSONCPP_1.8.0_ios_Debug_iphonesimulator.tar.gz?dl=0"
+  "thirdparty/jsoncpp/lib/ios/Debug-iphonesimulator"
+  "thirdparty/jsoncpp/lib/ios/Debug\$(EFFECTIVE_PLATFORM_NAME)"
+  )
+DOWNLOAD_STATIC_DEBUG_LIBRARY_BINARY_FILES("nanovg"
+  "https://www.dropbox.com/s/ml94aq71lmgxg73/LIBNANOVG_ios_Debug_iphonesimulator.tar.gz?dl=0"
+  "thirdparty/nanovg/lib/ios/Debug-iphonesimulator"
+  "thirdparty/nanovg/lib/ios/Debug\$(EFFECTIVE_PLATFORM_NAME)"
+  )
+DOWNLOAD_STATIC_DEBUG_LIBRARY_BINARY_FILES("ogg"
+  "https://www.dropbox.com/s/v4xxkan07c7vdg1/LIBOGG_1.3.2_ios_Debug_iphonesimulator.tar.gz?dl=0"
+  "thirdparty/ogg/lib/ios/Debug-iphonesimulator"
+  "thirdparty/ogg/lib/ios/Debug\$(EFFECTIVE_PLATFORM_NAME)"
+  )
+DOWNLOAD_STATIC_DEBUG_LIBRARY_BINARY_FILES("vorbis"
+  "https://www.dropbox.com/s/v60k8d8jzxtl0aq/LIBVORBIS_1.3.5_ios_Debug_iphonesimulator.tar.gz?dl=0"
+  "thirdparty/vorbis/lib/ios/Debug-iphonesimulator"
+  "thirdparty/vorbis/lib/ios/Debug\$(EFFECTIVE_PLATFORM_NAME)"
+  )
+
+
+
+
 
 IF( ${${CMAKE_PROJECT_NAME}_SOUND_PLATFORM} STREQUAL "fmod" )
   message(STATUS "need to add fmod lib")
