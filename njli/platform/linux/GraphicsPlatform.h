9bc38f8f60a54d79340a1285e1c325309b1e29be
James Folk <james.folk@envrmnt.com>
2018-07-24 22:36:14 -0400

    added the graphics platform for linux

--- njli/platform/linux/GraphicsPlatform.h
+++ njli/platform/linux/GraphicsPlatform.h
@@ -1,18 +1,17 @@
 
-#if defined(NJLI_GL_ES2)
-    #error Unsupported choice setting
-#elif defined(NJLI_GL_ES3)
-    #error Unsupported choice setting
-#elif defined(NJLI_GL_2)
+#if defined(__GL_ES2__)
+#error Unsupported choice setting
+#elif defined(__GL_ES3__)
+#error Unsupported choice setting
+#elif defined(__GL_2__)
   #define GL_GLEXT_PROTOTYPES
     #include <GL/gl.h>
     #include <GL/glext.h>
     #define NANOVG_GL2_IMPLEMENTATION
-#elif defined(NJLI_GL_3)
+#elif defined(__GL_3__)
     #include <OpenGL/gl3.h>
     #include <OpenGL/gl3ext.h>
     #define NANOVG_GL3_IMPLEMENTATION
 #else
-    #error Unsupported choice setting
+#error Unsupported choice setting
 #endif
-
