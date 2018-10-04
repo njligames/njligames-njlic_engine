6aa0882cf940836336360abbb4961d8c8cc4a62b
James Folk <james@njligames.com>
2018-10-04 17:07:29 -0400

    opengl3 works

--- njli/platform/Engine.cpp
+++ njli/platform/Engine.cpp
@@ -1328,10 +1328,39 @@ namespace njli
     SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
     SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
 
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
-
+      
+      
+#if defined(__GL_ES2__)
+      
+      SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
+      
+#elif defined(__GL_ES3__)
+      
+      SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+      
+#elif defined(__GL_2__)
+      
+      SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
 #if defined(__MACOSX__)
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+      SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+#endif
+      
+#elif defined(__GL_3__)
+      
+      SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+#if defined(__MACOSX__)
+//      SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL, 1);
+//      SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG);
+//      SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+//      SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
+//
+      
+//      SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+#endif
+      
+#endif
+      
+#if defined(__MACOSX__) || defined(__WINDOWS32__) || defined(__WINDOWS64__)
     SDL_EventState(SDL_DROPFILE, SDL_ENABLE);
 #endif
 
