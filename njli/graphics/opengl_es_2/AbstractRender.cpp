ea622942a5dc808f879189e415c7aae7b10f5658
James Folk <james@njligames.com>
2018-10-19 23:33:00 -0400

    only printing an error when there is an error

--- njli/graphics/opengl_es_2/AbstractRender.cpp
+++ njli/graphics/opengl_es_2/AbstractRender.cpp
@@ -68,11 +68,11 @@ void CheckOpenGLError(const char *stmt, const char *fname, int line)
 {
     int error = glGetError();
     
-    SDL_LogError(SDL_LOG_CATEGORY_TEST, "glGetError = %d, (%s) at %s:%d - for %s", error,
-                 GetOpenGLErrorString(error), fname, line, stmt);
-    
-//    SDL_assertPrint((error != GL_NO_ERROR), "glGetError = %d, (%s) at %s:%d - for %s", error,
-//                    GetOpenGLErrorString(error), fname, line, stmt);
+    if(error != GL_NO_ERROR)
+    {
+      SDL_LogError(SDL_LOG_CATEGORY_TEST, "glGetError = %d, (%s) at %s:%d - for %s", error,
+                   GetOpenGLErrorString(error), fname, line, stmt);
+    }
 }
 
 void initGL()
