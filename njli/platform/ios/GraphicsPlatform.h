9080eb9078ac41dae99558782abbcf0d9394651a
James Folk <james@njligames.com>
2018-11-01 22:40:42 -0400

    generalized the extensions

--- njli/platform/ios/GraphicsPlatform.h
+++ njli/platform/ios/GraphicsPlatform.h
@@ -1,8 +1,14 @@
 
 #if defined(__GL_ES2__)
+    #define glGenVertexArrays_NJLIC glGenVertexArraysOES
+    #define glBindVertexArray_NJLIC glBindVertexArrayOES
+    #define glDeleteVertexArrays_NJLIC glDeleteVertexArraysOES
+
     #include <OpenGLES/ES2/glext.h>
     #include <OpenGLES/ES2/gl.h>
+
     #define NANOVG_GLES2_IMPLEMENTATION
+
 #elif defined(__GL_ES3__)
     #include <OpenGLES/ES3/glext.h>
     #include <OpenGLES/ES3/gl.h>
@@ -14,7 +20,6 @@
 #else
     #error Unsupported choice setting
 #endif
-
 
 //https://www.khronos.org/opengles/sdk/docs/
 //https://www.khronos.org/registry/gles/specs/2.0/GLSL_ES_Specification_1.0.17.pdf
