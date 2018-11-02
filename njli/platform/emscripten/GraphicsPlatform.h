f25d8a135f168b9945a8625263d661f0fb22f44d
James Folk <james@njligames.com>
2018-11-01 23:09:50 -0400

    elia runs for emscripten

--- njli/platform/emscripten/GraphicsPlatform.h
+++ njli/platform/emscripten/GraphicsPlatform.h
@@ -1,9 +1,15 @@
 
 #if defined(__GL_ES2__)
+    #define glGenVertexArrays_NJLIC glGenVertexArraysOES
+    #define glBindVertexArray_NJLIC glBindVertexArrayOES
+    #define glDeleteVertexArrays_NJLIC glDeleteVertexArraysOES
+
     #define GL_GLEXT_PROTOTYPES
+
     #include <GLES2/gl2.h>
     #include <GLES2/gl2ext.h>
     #include <GLES2/gl2platform.h>
+
     #define NANOVG_GLES2_IMPLEMENTATION
 #elif defined(__GL_ES3__)
     #include <GLES3/gl3.h>
