169ffa6de76b3591c88e88fc9400567384bd1099
James Folk <james@njligames.com>
2018-12-03 14:15:21 -0500

    appletv is now using the NJLIC extensionfor opengl

--- njli/platform/appletv/GraphicsPlatform.h
+++ njli/platform/appletv/GraphicsPlatform.h
@@ -1,18 +1,24 @@
 
 #if defined(__GL_ES2__)
-    #include <OpenGLES/ES2/glext.h>
-    #include <OpenGLES/ES2/gl.h>
-    #define NANOVG_GLES2_IMPLEMENTATION
+#define glGenVertexArrays_NJLIC glGenVertexArraysOES
+#define glBindVertexArray_NJLIC glBindVertexArrayOES
+#define glDeleteVertexArrays_NJLIC glDeleteVertexArraysOES
+
+#include <OpenGLES/ES2/glext.h>
+#include <OpenGLES/ES2/gl.h>
+
+#define NANOVG_GLES2_IMPLEMENTATION
+
 #elif defined(__GL_ES3__)
-    #include <OpenGLES/ES3/glext.h>
-    #include <OpenGLES/ES3/gl.h>
-    #define NANOVG_GLES3_IMPLEMENTATION
+#include <OpenGLES/ES3/glext.h>
+#include <OpenGLES/ES3/gl.h>
+#define NANOVG_GLES3_IMPLEMENTATION
 #elif defined(__GL_2__)
-    #error Unsupported choice setting
+#error Unsupported choice setting
 #elif defined(__GL_3__)
-    #error Unsupported choice setting
+#error Unsupported choice setting
 #else
-    #error Unsupported choice setting
+#error Unsupported choice setting
 #endif
 
 
