9e88c143182ce26b1a18e67691f9b9617e57095a
James Folk <james@njligames.com>
2019-10-29 15:01:26 -0400

    appletv builds

---
+++ njli/platform/appletv/SoundPlatform.mm
@@ -0,0 +1,29 @@
+
+#include "SoundPlatform.h"
+
+#import <AVFoundation/AVAudioSession.h>
+#import <Foundation/Foundation.h>
+
+bool setupSoundPlatform()
+{
+  BOOL success = NO;
+  NSError *error = nil;
+
+  AVAudioSession *session = [AVAudioSession sharedInstance];
+
+  success = [session setCategory:AVAudioSessionCategoryAmbient error:&error];
+  if (!success)
+    {
+      NSLog(@"Error activating session: %@", [error localizedDescription]);
+      return false;
+    }
+
+  success = [session setActive:YES error:&error];
+  if (!success)
+    {
+      NSLog(@"Error activating session: %@", [error localizedDescription]);
+      return false;
+    }
+
+  return true;
+}
