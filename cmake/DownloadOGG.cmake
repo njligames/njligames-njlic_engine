2b0efae70615f316224ff1d3b7ce458914b63c52
James Folk <jamesfolk@Jamess-MacBook-Pro.local>
2021-08-17 21:04:24 -0400

    updated the ogg links

--- cmake/DownloadOGG.cmake
+++ cmake/DownloadOGG.cmake
@@ -1,6 +1,6 @@
 set(LIBRARY_NAME "ogg")
 
-set(INCLUDE_URL "https://www.dropbox.com/s/agc7zmqg9m64x9i/libogg_includes.tar.gz?dl=0")
+set(INCLUDE_URL "https://www.dropbox.com/s/bj1tm06uzaqq7o7/libogg_includes.tar.gz?dl=0")
 
 set(EMSCRIPTEN_DEBUG_URL "https://www.dropbox.com/s/tqvf2039tjjqpb6/libogg_emscripten_Debug.tar.gz?dl=0")
 set(EMSCRIPTEN_RELEASE_URL "https://www.dropbox.com/s/rp5yob3wabc9vcn/libogg_emscripten_Release.tar.gz?dl=0")
@@ -15,14 +15,14 @@ set(WINDOWS32_RELWITHDEBINFO_URL "https://www.dropbox.com/s/7h5jygomq7xcyc1/libo
 set(WINDOWS64_DEBUG_URL "https://www.dropbox.com/s/x7ult88qm3g3cgv/libogg_windows64_Debug.tar.gz?dl=0")
 set(WINDOWS64_RELEASE_URL "https://www.dropbox.com/s/8969b6opmrlam3f/libogg_windows64_Release.tar.gz?dl=0")
 
-set(IOS_DEVICE_DEBUG_URL "https://www.dropbox.com/s/wn2w8itwe1v7sv4/libogg_ios_Debug-iphoneos.tar.gz?dl=0")
-set(IOS_SIMULATOR_DEBUG_URL "https://www.dropbox.com/s/6qjv1hr0twiv7gx/libogg_ios_Debug-iphonesimulator.tar.gz?dl=0")
-set(IOS_DEVICE_MINSIZEREL_URL "https://www.dropbox.com/s/aq3k1cpclm5ee4r/libogg_ios_MinsizeRel-iphoneos.tar.gz?dl=0")
-set(IOS_SIMULATOR_MINSIZEREL_URL "https://www.dropbox.com/s/u5yk6vhudhmbcds/libogg_ios_MinsizeRel-iphonesimulator.tar.gz?dl=0")
-set(IOS_DEVICE_RELEASE_URL "https://www.dropbox.com/s/vwtsoeodtzdwf62/libogg_ios_Release-iphoneos.tar.gz?dl=0")
-set(IOS_SIMULATOR_RELEASE_URL "https://www.dropbox.com/s/kmr72e2uh1cdcn8/libogg_ios_Release-iphonesimulator.tar.gz?dl=0")
-set(IOS_DEVICE_RELWITHDEBINFO_URL "https://www.dropbox.com/s/rheo2ueu8qy76rs/libogg_ios_RelWithDebugInfo-iphoneos.tar.gz?dl=0")
-set(IOS_SIMULATOR_RELWITHDEBINFO_URL "https://www.dropbox.com/s/gmw07emc08vxtja/libogg_ios_RelWithDebugInfo-iphonesimulator.tar.gz?dl=0")
+set(IOS_DEVICE_DEBUG_URL "https://www.dropbox.com/s/24kmj1n52sb5u8h/libogg_ios_Debug-iphoneos.tar.gz?dl=0")
+set(IOS_SIMULATOR_DEBUG_URL "https://www.dropbox.com/s/2lnkr9titv515ho/libogg_ios_Debug-iphonesimulator.tar.gz?dl=0")
+set(IOS_DEVICE_MINSIZEREL_URL "https://www.dropbox.com/s/tecokkodp79nnpi/libogg_ios_MinsizeRel-iphoneos.tar.gz?dl=0")
+set(IOS_SIMULATOR_MINSIZEREL_URL "https://www.dropbox.com/s/g0l4rdeptkwr3ev/libogg_ios_MinsizeRel-iphonesimulator.tar.gz?dl=0")
+set(IOS_DEVICE_RELEASE_URL "https://www.dropbox.com/s/py10k0z3jbnmjgl/libogg_ios_Release-iphoneos.tar.gz?dl=0")
+set(IOS_SIMULATOR_RELEASE_URL "https://www.dropbox.com/s/uq51n7ekrb9x7pc/libogg_ios_Release-iphonesimulator.tar.gz?dl=0")
+set(IOS_DEVICE_RELWITHDEBINFO_URL "https://www.dropbox.com/s/m3rp6fso6oiz9zy/libogg_ios_RelWithDebugInfo-iphoneos.tar.gz?dl=0")
+set(IOS_SIMULATOR_RELWITHDEBINFO_URL "https://www.dropbox.com/s/6kf5jl3ji508mzs/libogg_ios_RelWithDebugInfo-iphonesimulator.tar.gz?dl=0")
 
 set(APPLETV_DEVICE_DEBUG_URL "https://www.dropbox.com/s/5ngl2398a7h3s8o/libogg_appletv_Debug-appletvos.tar.gz?dl=0")
 set(APPLETV_SIMULATOR_DEBUG_URL "https://www.dropbox.com/s/pvwpwxqj7zyct4j/libogg_appletv_Debug-appletvsimulator.tar.gz?dl=0")
